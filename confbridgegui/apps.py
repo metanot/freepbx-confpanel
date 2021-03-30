5b552c6b9fc34fd72233ad5e8d749d56ee92b441
Dmitriev Vyacheslav <metanot@yandex.ru>
2021-03-30 13:12:26 +0000

    Fixed Skeleton of front and back

--- confbridgegui/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ConfbridgeguiConfig(AppConfig):
-    name = 'confbridgegui'
---
+++ confpanelgui/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ConfpanelguiConfig(AppConfig):
+    name = 'confpanelgui'
